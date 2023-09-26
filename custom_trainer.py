8e7bca9def0427cef5ad10c6e7c173800385631c
Itay Segev <itaysegev11@gmail.com>
2023-09-26 14:50:53 +0300

    Fix some bugs

---
+++ custom_trainer.py
@@ -0,0 +1,172 @@
+from typing import List, Mapping
+from datetime import datetime
+from citylearn.citylearn import CityLearnEnv
+from citylearn.reward_function import RewardFunction
+from citylearn.data import DataSet
+from citylearn.wrappers import NormalizedObservationWrapper
+from citylearn.wrappers import StableBaselines3Wrapper
+import matplotlib.pyplot as plt
+
+from simulation_setup import set_schema_buildings, set_schema_simulation_period, set_active_observations
+from utils import CustomCallback, get_loader
+from simulation_results import plot_actions, plot_rewards, plot_simulation_summary
+# baseline RL algorithms
+from stable_baselines3 import SAC
+
+
+def train_your_custom_sac(
+    agent_kwargs: dict, episodes: int, reward_function: RewardFunction,
+    building_count: int, day_count: int, active_observations: List[str],
+    random_seed: int, reference_envs: Mapping[str, CityLearnEnv] = None,
+    show_figures: bool = None
+) -> dict:
+    """Trains a custom soft-actor critic (SAC) agent on a custom environment.
+
+    Trains an SAC agent using a custom environment and agent hyperparamter
+    setup and plots the key performance indicators (KPIs), actions and
+    rewards from training and evaluating the agent.
+
+    Parameters
+    ----------
+    agent_kwargs: dict
+        Defines the hyperparameters used to initialize the SAC agent.
+    episodes: int
+        Number of episodes to train the agent for.
+    reward_function: RewardFunction
+        A base or custom reward function class.
+    building_count: int
+        Number of buildings to set as active in schema.
+    day_count: int
+        Number of simulation days.
+    active_observations: List[str]
+        Names of observations to set active to be passed to control agent.
+    random_seed: int
+        Seed for pseudo-random number generator.
+    reference_envs: Mapping[str, CityLearnEnv], default: None
+        Mapping of user-defined control agent names to environments
+        the agents have been used to control.
+    show_figures: bool, default: False
+        Indicate if summary figures should be plotted at the end of
+        evaluation.
+
+    Returns
+    -------
+    result: dict
+        Results from training the agent as well as some input variables
+        for reference including the following value keys:
+
+            * random_seed: int
+            * env: CityLearnEnv
+            * model: SAC
+            * actions: List[float]
+            * rewards: List[float]
+            * agent_kwargs: dict
+            * episodes: int
+            * reward_function: RewardFunction
+            * buildings: List[str]
+            * simulation_start_time_step: int
+            * simulation_end_time_step: int
+            * active_observations: List[str]
+            * train_start_timestamp: datetime
+            * train_end_timestamp: datetime
+    """
+
+    # get schema
+    schema = DataSet.get_schema('citylearn_challenge_2022_phase_all')
+
+    # select buildings
+    schema, buildings = set_schema_buildings(
+        schema, building_count, random_seed
+    )
+    print('Selected buildings:', buildings)
+
+    # select days
+    schema, simulation_start_time_step, simulation_end_time_step =\
+        set_schema_simulation_period(schema, day_count, random_seed)
+    print(
+        f'Selected {day_count}-day period time steps:',
+        (simulation_start_time_step, simulation_end_time_step)
+    )
+
+    # set active observations
+    schema = set_active_observations(schema, active_observations)
+    print(f'Active observations:', active_observations)
+
+    # initialize environment
+    env = CityLearnEnv(schema, central_agent=True)
+
+    # set reward function
+    env.reward_function = reward_function(env=env)
+
+    # wrap environment
+    env = NormalizedObservationWrapper(env)
+    env = StableBaselines3Wrapper(env)
+
+    # initialize agent
+    model = SAC('MlpPolicy', env, **agent_kwargs, seed=random_seed)
+
+    # initialize loader
+    total_timesteps = episodes*(env.time_steps - 1)
+    print('Number of episodes to train:', episodes)
+    loader = get_loader(max=total_timesteps)
+    # display(loader)
+
+    # initialize callback
+    callback = CustomCallback(env=env, loader=loader)
+
+    # train agent
+    train_start_timestamp = datetime.utcnow()
+    model = model.learn(total_timesteps=total_timesteps, callback=callback)
+    train_end_timestamp = datetime.utcnow()
+
+    # evaluate agent
+    observations = env.reset()
+    actions_list = []
+
+    while not env.done:
+        actions, _ = model.predict(observations, deterministic=True)
+        observations, _, _, _ = env.step(actions)
+        actions_list.append(actions)
+
+    # get rewards
+    rewards = callback.reward_history[:episodes]
+
+    # plot summary and compare with other control results
+    if show_figures is not None and show_figures:
+        env_id = 'Your-SAC'
+
+        if reference_envs is None:
+            reference_envs = {env_id: env}
+        else:
+            reference_envs = {env_id: env, **reference_envs}
+
+        plot_simulation_summary(reference_envs)
+
+        # plot actions
+        plot_actions(actions_list, f'{env_id} Actions', env)
+
+        # plot rewards
+        _, ax = plt.subplots(1, 1, figsize=(5, 2))
+        ax = plot_rewards(ax, rewards, f'{env_id} Rewards')
+        plt.tight_layout()
+        plt.show()
+
+    else:
+        pass
+
+    return {
+        'random_seed': random_seed,
+        'env': env,
+        'model': model,
+        'actions': actions_list,
+        'rewards': rewards,
+        'agent_kwargs': agent_kwargs,
+        'episodes': episodes,
+        'reward_function': reward_function,
+        'buildings': buildings,
+        'simulation_start_time_step': simulation_start_time_step,
+        'simulation_end_time_step': simulation_end_time_step,
+        'active_observations': active_observations,
+        'train_start_timestamp': train_start_timestamp,
+        'train_end_timestamp': train_end_timestamp,
+    }
